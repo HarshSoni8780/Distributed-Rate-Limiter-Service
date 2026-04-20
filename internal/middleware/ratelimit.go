4191be401ee8d102ecd0dfafcbb1414ba73ccdf1
Harsh <harsh@Harshs-Macbook-Air.local>
2026-04-21 00:56:32 +0530

    rate-limit

--- internal/middleware/ratelimit.go
+++ internal/middleware/ratelimit.go
@@ -1,1 +1,23 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"rate-limiter/internal/limiter"
+)
+
+func RateLimimt(l *limiter.FixedWindow) gin.HandlerFunc{
+	return func(c *gin.Context){
+		user := c.ClientIP()
+		
+		if !l.Allow(user){
+			c.JSON(http.StatusTooManyRequests, gin.H{
+				"error": "rate limit exceeded",
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
